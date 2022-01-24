8b00e47728bcf95335c73b65d38ccc0c39b29f78
Adeenfasihi <adeenfasihi@hotmail.com>
2022-01-24 22:21:13 +0100

    add runner.py and text and csv files

---
+++ runner.py
@@ -0,0 +1,33 @@
+import csv
+import subprocess, os
+
+benchmarks = os.listdir('benchmarks')
+args = ["python3", "pyres-fof.py", "-tifbp", "-H", "-n", "benchmarks/"]
+clauses = ["PickGiven5_FIFO", "PickGiven5_FILO", "PickGiven5_Horn", "PickGiven5_Unit"]
+n = ["smallest", "largest"]
+
+
+with open('file.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    for i in benchmarks:
+        for j in clauses:
+            for k in n:
+                args[3] += j
+                args[4] += k
+                args[5] += i
+                
+                print(args)
+                temp = None
+                with open('file.txt', 'w+') as fh:
+                    subprocess.Popen(args=args, stdout=fh).wait()
+                    fh.seek(0)
+                    temp = fh.readlines()[-11:]
+                    temp = [s.strip() for s in temp]
+                    temp = [s.strip('# ') for s in temp]
+
+                args = ["python3", "pyres-fof.py", "-tifbp", "-H", "-n", "benchmarks/"]
+                writer.writerow([i, j, k, temp])
+
+                break
+            break
+        break
